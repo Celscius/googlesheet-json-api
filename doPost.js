634afeee6e860ca3fdb0f7b55030056390a64afb
Dcw0818 <60767284+Celscius@users.noreply.github.com>
2024-03-20 15:59:41 +0800

    Create doPost.js

---
+++ doPost.js
@@ -0,0 +1,37 @@
+function doGet(e) {
+
+    var sheet = SpreadsheetApp.getActive().getSheetByName('image')
+    const lastRow = sheet.getLastRow()
+    const lastColumn = sheet.getLastColumn()
+
+    var rng = sheet.getRange(1, 1, lastRow, lastColumn)
+    var vals = rng.getValues()
+
+    //change value from array into object
+    convert_to_object = (array) => {
+        const [headers, ...data] = array;
+
+        const Object = data.map(row => {
+            return row.reduce((acc, value, i) => {
+                const key = headers[i];
+                // console.log({...acc});
+                if (key === '') return acc;
+                return {
+                    ...acc,
+                    [key]: value
+                };
+            }, {});
+        });
+        return Object
+    }
+    let cell = sheet.getRange("K13").getValue()
+    const data = {
+      //"time": currenttime(),
+      //"latest_data": currenttime(),
+        "cell": lastColumn,
+        "data": convert_to_object(vals)
+    }
+
+    Logger.log(data)
+    return ContentService.createTextOutput(JSON.stringify(data)).setMimeType(ContentService.MimeType.JSON);
+}
